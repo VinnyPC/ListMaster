c010cc14f8bb66896b3ee1016c8d55297240104c
VinnyPC <vinisilvax3@gmail.com>
2023-06-18 20:05:56 -0300

    feat: Criação do projeto

---
+++ listMaster/listMaster/src/main/java/com/listmaster/listMaster/ListMasterApplication.java
@@ -0,0 +1,13 @@
+package com.listmaster.listMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListMasterApplication.class, args);
+	}
+
+}
---
+++ listMaster/listMaster/src/test/java/com/listmaster/listMaster/ListMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listmaster.listMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
