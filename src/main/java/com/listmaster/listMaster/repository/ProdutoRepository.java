124b4b851b55758817ff343138f05a27c4eed901
VinnyPC <vinisilvax3@gmail.com>
2023-07-08 16:46:23 -0300

    feat: put,delete dos produtos/relacionamento produto>categoria

---
+++ src/main/java/com/listmaster/listMaster/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.listmaster.listMaster.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.listmaster.listMaster.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
